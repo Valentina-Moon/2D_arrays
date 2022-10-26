d14b4b9ef64de97e6dc2698133b27b531a9dd14e
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-26 16:51:05 +0300

    matrix/getlenght

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// task 1 с числами.
+
+
+
+int[,] matrix = new int[3, 4];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j =0; j <matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]}");
+    }
+Console.WriteLine();
+}
